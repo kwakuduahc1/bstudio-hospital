507f4e7c87ff012aa9c4bdda151d179ee853c93f
Duah Kwaku <ibrahimkwakuduah@hotmail.com>
2018-12-25 19:54:26 +0000

    vitals signs controller

---
+++ WinClinic/Controllers/OPD/VitalSignsController.cs
@@ -0,0 +1,57 @@
+﻿using bStudioHospital.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using WinClinic.DTOs.Records;
+using bsopd = bStudioHospital.Model.OPD;
+
+namespace WinClinic.Controllers.OPD
+{
+    public class VitalSignsController : Controller
+    {
+        private readonly VitalsHelper db;
+
+        public VitalSignsController(DbContextOptions<DataContext> dbContextOptions)
+        {
+            db = new VitalsHelper(dbContextOptions);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable> List(byte num, byte off)
+        {
+            var list = await db.List(num, off);
+            return list.Select(x => new { x.Patients.FullName, x.Temperature, x.Systolic, x.Diastolic, x.Respiration, x.Weight, x.DateSeen, x.Pulse, x.ID, x.UserName });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Patient(string id)
+        {
+            var pat = await db.Patient(id);
+            if (pat == null)
+                return NotFound(new { Message = $"{id} does not match any known record" });
+            return Ok(new { pat.FullName, pat.PatientsID, pat.SchemesID, pat.DateOfBirth });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Find(Guid id)
+        {
+            bsopd.OPD opd = await db.Find(id);
+            if (opd == null)
+                return BadRequest(new { Message = "Record was not found" });
+            return Ok(opd);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]bsopd.OPD opd)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new { Error = "Invalid data was submitted", Message = ModelState.Values.First(x => x.Errors.Count > 0).Errors.Select(t => t.ErrorMessage).First() });
+            db.Add(opd);
+            await db.Save();
+            return Created($"/OPD/Find?id={opd.ID}", opd);
+        }
+    }
+}
---
+++ WinClinic/DTOs/Vitals/VitalsHelper.cs
@@ -0,0 +1,56 @@
+﻿using bStudioHospital.Model;
+using bStudioHospital.Model.OPD;
+using bStudioHospital.Model.Records;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WinClinic.Model.ViewModels;
+
+namespace WinClinic.DTOs.Records
+{
+    public class VitalsHelper
+    {
+        readonly DataContext db;
+
+        public VitalsHelper(DbContextOptions<DataContext> context) => db = new DataContext(context);
+
+        /// <summary>
+        /// Checks whether the given opd number of the patient already exists
+        /// </summary>
+        /// <param name="id">The index number, staff id or other unique identifier of the patient</param>
+        /// <returns></returns>
+        public Task<bool> CheckIDExists(string id) => Task.Run(async () => await db.Patients.AnyAsync(t => t.PatientsID == id));
+
+
+        /// <summary>
+        /// Add new vital signs
+        /// </summary>
+        /// <param name="opd"></param>
+        public void Add(OPD opd)
+        {
+            opd.DateSeen = DateTime.Now;
+            opd.ID = Guid.NewGuid();
+            db.Add(opd);
+        }
+
+        /// <summary>
+        /// Save changes
+        /// </summary>
+        /// <returns></returns>
+        public Task<int> Save() => Task.Run(async () => await db.SaveChangesAsync());
+
+        /// <summary>
+        /// Gets the list of patients
+        /// </summary>
+        /// <param name="num">The number to fetch</param>
+        /// <param name="off">The number to skip or offset</param>
+        /// <returns>List of patients</returns>
+        public Task<List<OPD>> List(byte num, byte off) => Task.Run(async () => await db.OpdHistory.OrderByDescending(x => x.DateSeen).Skip(off).Take(num).Include(x => x.Patients).ToListAsync());
+
+        public Task<OPD> Find(Guid id) => Task.Run(async () => await db.OpdHistory.FindAsync(id));
+
+        public Task<Patients> Patient(string id) => Task.Run(async () => await db.Patients.SingleOrDefaultAsync(x => x.PatientsID == id));
+    }
+}
