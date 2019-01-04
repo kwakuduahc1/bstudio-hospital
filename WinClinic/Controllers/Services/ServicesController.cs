47cea1083d301520bd53145c95540a35c1913b5d
Duah Kwaku <ibrahimkwakuduah@hotmail.com>
2019-01-04 06:39:59 +0000

    services controller

---
+++ WinClinic/Controllers/Services/ServicesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WinClinic.DTOs;
+using WinClinic.Model;
+using WinClinic.Model.Services;
+
+namespace WinClinic.Controllers.Services
+{
+    public class ServicesController : Controller
+    {
+        private readonly ServicesHelper db;
+
+        public ServicesController(DbContextOptions<DataContext> dbContextOptions) => db = new ServicesHelper(dbContextOptions);
+
+        [HttpGet]
+        public async Task<IEnumerable> List(Guid id) =>await db.GetServices(id);
+
+        [HttpPost]
+        public async Task<IActionResult> Serve([FromBody]List<PatientServices> services)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new { Error = "Invalid data was submitted", Message = ModelState.Values.First(x => x.Errors.Count > 0).Errors.Select(t => t.ErrorMessage).First() });
+            db.Serve(services, User.Identity.Name);
+            await db.Save();
+            return Accepted();
+        }
+    }
+}
---
+++ WinClinic/DTOs/ServicesHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WinClinic.Model;
+using WinClinic.Model.Services;
+
+namespace WinClinic.DTOs
+{
+    public class ServicesHelper
+    {
+        readonly DataContext db;
+
+        public ServicesHelper(DbContextOptions<DataContext> context) => db = new DataContext(context);
+
+        public Task<IEnumerable> GetServices(Guid id) => Task.Run<IEnumerable>(async () => await db.PatientServices.Where(x => x.PatientAttendanceID == id && !x.IsServed).Select(x => new { x.DateRequested, x.ServiceCodes.ServiceCode, x.ServiceCodes.Services.Service, x.ServiceCodesID, x.Frequency, x.PatientServicesID, x.NumberOfDays, x.PatientAttendanceID }).ToListAsync());
+
+        public void Serve(List<PatientServices> services, string user)
+        {
+            services.ForEach(x =>
+            {
+                var drug = db.PatientServices.Find(x.PatientServicesID);
+                if (drug != null)
+                {
+                    drug.IsServed = x.IsServed;
+                    x.DateServed = DateTime.Now;
+                    x.ServingOficcer = user;
+                    db.Entry(drug).State = EntityState.Modified;
+                }
+            });
+        }
+
+        public Task<int> Save() => Task.Run(async () => await db.SaveChangesAsync());
+    }
+}
